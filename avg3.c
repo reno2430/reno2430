24f8941220f961e80cb4cf4aec2383c93ba4f655
reno2430 <renorobert777@gmail.com>
2024-10-17 12:13:56 +0530

    Add files via upload

---
+++ avg3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    float a,b,c,avg;
+    printf("Enter the two number");
+    scanf("%f%f%f",&a,&b,&c);
+    avg=(a+b+c)/3;
+    printf("%f",avg);
+    return 0;
+}
+
