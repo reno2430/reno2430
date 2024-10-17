2c820d311f10996655fb88689992eb1d8c73e0c1
reno2430 <renorobert777@gmail.com>
2024-10-17 11:46:38 +0530

    Add files via upload

---
+++ sum2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter the first number");
+	scanf("%d",&a);
+	printf("enter the second number");
+	scanf("%d",&b);
+	c=a+b;
+	printf("sum= %d",c);
+	return 0;
+}
